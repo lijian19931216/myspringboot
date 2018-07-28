fa9eda934f087ee3bc0593f4de2a484de3f79f1b
上海陆鹰-李剑 <928229131@qq.com>
2018-07-28 19:04:14 +0800

    test分支提交

--- src/main/java/com/luying/MyspringbootApplication.java
+++ src/main/java/com/luying/MyspringbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyspringbootApplication {
+public class MyspringbootApplication {//test
 
     public static void main(String[] args) {
         SpringApplication.run(MyspringbootApplication.class, args);
